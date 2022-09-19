63f6583fbab340aa78d0f37450c5ab35b25338d9
Oren <orennoyes@Administrators-MacBook-Pro.local>
2022-09-19 15:47:44 -0600

    commit tree c070ceff1e857aeb36f7c8243b761f3760d9b125

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper';
+const currentUser2 = 'GRACE HOPPER';
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser2}!`;
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const shortGreeting = `Welcome, G !`;
+
